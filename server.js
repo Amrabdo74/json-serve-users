bb52eaeb3a37c13925863ff20dcaa73a7727b573
Amrabdo74 <andkpe12@gmail.com>
2025-01-21 16:41:39 +0200

    amr

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+// server.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+// Enable CORS for all origins
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    next();
+});
+
 server.use(middlewares);
 server.use(router);
 
-const PORT = 3000;
+// Use environment variable for port or default to 3000
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+    console.log(`JSON Server is running on port ${PORT}`);
 });
